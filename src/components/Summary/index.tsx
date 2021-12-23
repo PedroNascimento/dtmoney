61115a22004dad09d5991138b992b8c60bcb4640
Pedro Nascimento <contato@pedroanascimento.com>
2021-12-22 22:25:12 -0300

    :tada: First commit

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,72 @@
+import { useTransactions } from '../../hooks/UseTransactions';
+
+import incomeImg from '../../assets/entradas.svg'
+import outcomeImg from '../../assets/saidas.svg'
+import totalImg from '../../assets/total.svg'
+
+import { Container } from "./styles";
+
+export function Summary(){
+const { transactions } = useTransactions()
+
+
+const summary = transactions.reduce((acc, transaction) => { 
+  if (transaction.type === 'deposit'){
+    acc.deposits += transaction.amount;
+    acc.total += transaction.amount;
+  } else{
+    acc.withdraws += transaction.amount;
+    acc.total -= transaction.amount;
+  }
+
+  return acc
+
+}, {
+  deposits: 0,
+  withdraws: 0,
+  total: 0,
+})
+
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas"></img>
+        </header>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.deposits)}
+        </strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Saídas"></img>
+        </header>
+        <strong> - 
+         {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.withdraws)}          
+        </strong>
+      </div>
+
+      <div className='hightlight-background'>
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total"></img>
+        </header>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.total)} 
+        </strong>
+      </div>
+    </Container>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createServer, Model } from 'miragejs'
+import {App} from './App';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freela Website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 8000,
+          createAt: new Date('2021-05-12 12:05:32'),
+        },
+        {
+          id: 2,
+          title: 'Gasolina do Carro',
+          type: 'withdraw',
+          category: 'Combustível',
+          amount: 150,
+          createAt: new Date('2021-12-12 15:05:32'),
+        }
+    
+      ],
+    })
+  },
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    })
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
+    
+    })
+  }
+})
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +55,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
